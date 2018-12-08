0a33c5285f68ac71f9f6088eb7abe1e3d76d1489
Rida Kadri <ridakadri14@gmail.com>
2018-12-08 11:21:15 +0530

    changes in model

---
+++ lstm.py
@@ -0,0 +1,118 @@
+from __future__ import print_function
+import argparse
+import os
+import pickle
+from tensorflow.python.keras.callbacks import TensorBoard, LambdaCallback
+# from tensorflow.python.keras.utils import plot_model
+from utils import read_dataset
+from custom_metrics import f1
+from attention_layer import Attention
+import tensorflow as tf
+from tensorflow.python.keras import initializers, regularizers, optimizers, backend as K
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.models import Model, load_model
+from tensorflow.python.keras.layers import Input, Dense, Activation, LSTM, Embedding, Bidirectional, Lambda, Flatten
+
+
+class RecurrentAttentionMemory:
+    def __init__(self):
+        self.HOPS = 5
+        self.SCORE_FUNCTION = 'mlp'  # scaled_dot_product / mlp (concat) / bi_linear (general dot)
+        self.DATASET = 'restaurant'  # 'twitter', 'restaurant', 'laptop'
+        self.POLARITIES_DIM = 3
+        self.EMBEDDING_DIM = 300
+        self.LEARNING_RATE = 0.001
+        self.INITIALIZER = initializers.RandomUniform(minval=-0.05, maxval=0.05)
+        self.REGULARIZER = regularizers.l2(0.001)
+        self.LSTM_PARAMS = {
+            'units': 200,
+            'activation': 'tanh',
+            'recurrent_activation': 'sigmoid',
+            'kernel_initializer': self.INITIALIZER,
+            'recurrent_initializer': self.INITIALIZER,
+            'bias_initializer': self.INITIALIZER,
+            'kernel_regularizer': self.REGULARIZER,
+            'recurrent_regularizer': self.REGULARIZER,
+            'bias_regularizer': self.REGULARIZER,
+            'dropout': 0,
+            'recurrent_dropout': 0,
+        }
+        self.MAX_SEQUENCE_LENGTH = 80
+        self.MAX_ASPECT_LENGTH = 10
+        self.BATCH_SIZE = 8
+        self.EPOCHS = 5
+
+        self.texts_raw_indices, self.texts_raw_without_aspects_indices, self.texts_left_indices, self.texts_left_with_aspects_indices, \
+        self.aspects_indices, self.texts_right_indices, self.texts_right_with_aspects_indices, \
+        self.polarities_matrix, \
+        self.embedding_matrix, \
+        self.tokenizer = \
+            read_dataset(type=self.DATASET,
+                         mode='train',
+                         embedding_dim=self.EMBEDDING_DIM,
+                         max_seq_len=self.MAX_SEQUENCE_LENGTH, max_aspect_len=self.MAX_ASPECT_LENGTH)
+
+        if os.path.exists('ram_saved_model.h5'):
+            print('loading saved model...')
+            self.model = load_model('ram_saved_model.h5')
+        else:
+            print('Build model...')
+            inputs_sentence = Input(shape=(self.MAX_SEQUENCE_LENGTH,), name='inputs_sentence')
+            inputs_aspect = Input(shape=(self.MAX_ASPECT_LENGTH,), name='inputs_aspect')
+            nonzero_count = Lambda(lambda xin: tf.count_nonzero(xin, dtype=tf.float32))(inputs_aspect)
+            sentence = Embedding(input_dim=len(self.tokenizer.word_index) + 1,
+                          output_dim=self.EMBEDDING_DIM,
+                          input_length=self.MAX_SEQUENCE_LENGTH,
+                          mask_zero=True,
+                          weights=[self.embedding_matrix],
+                          trainable=False, name='sentence_embedding')(inputs_sentence)
+            aspect = Embedding(input_dim=len(self.tokenizer.word_index) + 1,
+                             output_dim=self.EMBEDDING_DIM,
+                             input_length=self.MAX_ASPECT_LENGTH,
+                             mask_zero=True,
+                             weights=[self.embedding_matrix],
+                             trainable=False, name='aspect_embedding')(inputs_aspect)
+            memory = Bidirectional(LSTM(return_sequences=True, **self.LSTM_PARAMS), name='memory')(sentence)
+            aspect = Bidirectional(LSTM(return_sequences=True, **self.LSTM_PARAMS), name='aspect')(aspect)
+            x = Lambda(lambda xin: K.sum(xin[0], axis=1) / xin[1], name='aspect_mean')([aspect, nonzero_count])
+            print("before",x.shape)
+            shared_attention = Attention(score_function=self.SCORE_FUNCTION,
+                                         initializer=self.INITIALIZER, regularizer=self.REGULARIZER,
+                                         name='shared_attention')
+            for i in range(self.HOPS):
+                x = shared_attention((memory, x))
+            x = Flatten()(x)
+            print(x.shape.ndims)
+            if(x.shape[1] != None):
+                x = Dense(self.POLARITIES_DIM)(x)
+            print(x.shape)
+            predictions = Activation('softmax')(x)
+            model = Model(inputs=[inputs_sentence, inputs_aspect], outputs=predictions)
+            model.summary()
+            model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(lr=self.LEARNING_RATE), metrics=['acc', f1])
+            # plot_model(model, to_file='model.png')
+            self.model = model
+
+    def train(self):
+        def modelSave(epoch, logs):
+            if (epoch + 1) % 5 == 0:
+                self.model.save('ram_saved_model.h5')
+        msCallBack = LambdaCallback(on_epoch_end=modelSave)
+
+        texts_raw_indices, texts_raw_without_aspects_indices, texts_left_indices, texts_left_with_aspects_indices, \
+        aspects_indices, texts_right_indices, texts_right_with_aspects_indices, \
+        polarities_matrix = \
+            read_dataset(type=self.DATASET,
+                         mode='test',
+                         embedding_dim=self.EMBEDDING_DIM,
+                         max_seq_len=self.MAX_SEQUENCE_LENGTH, max_aspect_len=self.MAX_ASPECT_LENGTH)
+
+        self.model.fit([self.texts_raw_indices, self.aspects_indices], self.polarities_matrix,
+                       epochs=self.EPOCHS, batch_size=self.BATCH_SIZE)
+        filename = 'finalized_model.sav'
+        pickle.dump(model, open(filename, 'wb'))
+
+
+if __name__ == '__main__':
+    model = RecurrentAttentionMemory()
+    model.train()
---
+++ utils.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+# file: utils.py
+# author: songyouwei <youwei0314@gmail.com>
+# Copyright (C) 2018. All Rights Reserved.
+
+import os
+import pickle
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+
+def load_word_vec(word_index=None, embedding_dim=100):
+    fname = './glove.twitter.27B/glove.twitter.27B.'+str(embedding_dim)+'d.txt' \
+        if embedding_dim != 300 else './glove.42B.300d/glove.42B.300d.txt'
+    fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    word_vec = {}
+    for line in fin:
+        tokens = line.rstrip().split()
+        if tokens[0] in word_index.keys():
+            word_vec[tokens[0]] = np.asarray(tokens[1:], dtype='float32')
+    return word_vec
+
+
+def read_dataset(type='twitter', mode='train', embedding_dim=100, max_seq_len=40, max_aspect_len=3, polarities_dim=3):
+    print("preparing data...")
+    fname = {
+        'twitter': {
+            'train': './datasets/acl-14-short-data/train.raw',
+            'test': './datasets/acl-14-short-data/test.raw'
+        },
+        'restaurant': {
+            'train': './datasets/semeval14/Restaurants_Train.xml.seg',
+            'test': './datasets/semeval14/Restaurants_Test_Gold.xml.seg'
+        },
+        'laptop': {
+            'train': './datasets/semeval14/Laptops_Train.xml.seg',
+            'test': './datasets/semeval14/Laptops_Test_Gold.xml.seg'
+        }
+    }
+    fin = open(fname[type][mode], 'r', encoding='utf-8', newline='\n', errors='ignore')
+    lines = fin.readlines()
+    fin.close()
+    print("number of {0} {1} data: {2}".format(type, mode ,len(lines)/3))
+
+    text = ''
+    texts_raw = []
+    texts_raw_without_aspects = []
+    texts_left = []
+    texts_left_with_aspects = []
+    texts_right = []
+    texts_right_with_aspects = []
+    aspects = []
+    polarities = []
+
+    for i in range(0, len(lines), 3):
+        text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
+        aspect = lines[i+1].lower().strip()
+        polarity = lines[i+2].strip()
+
+        text_raw = text_left + " " + aspect + " " + text_right
+        text += text_raw
+
+        texts_raw.append(text_raw)
+        texts_raw_without_aspects.append(text_left + " " + text_right)
+        texts_left.append(text_left)
+        texts_left_with_aspects.append(text_left + " " + aspect)
+        texts_right.append(text_right)
+        texts_right_with_aspects.append(aspect + " " + text_right)
+        aspects.append(aspect)
+        polarities.append(int(polarity))
+
+    text_words = text.strip().split()
+    print('tokenizing...')
+    tokenizer = Tokenizer(lower=False)
+    tokenizer.fit_on_texts(text_words)
+    word_index = tokenizer.word_index
+
+    texts_raw_indices = tokenizer.texts_to_sequences(texts_raw)
+    texts_raw_indices = pad_sequences(texts_raw_indices, maxlen=max_seq_len)
+    texts_raw_without_aspects_indices = tokenizer.texts_to_sequences(texts_raw_without_aspects)
+    texts_raw_without_aspects_indices = pad_sequences(texts_raw_without_aspects_indices, maxlen=max_seq_len)
+    texts_left_indices = tokenizer.texts_to_sequences(texts_left)
+    texts_left_indices = pad_sequences(texts_left_indices, maxlen=max_seq_len)
+    texts_left_with_aspects_indices = tokenizer.texts_to_sequences(texts_left_with_aspects)
+    texts_left_with_aspects_indices = pad_sequences(texts_left_with_aspects_indices, maxlen=max_seq_len)
+    texts_right_indices = tokenizer.texts_to_sequences(texts_right)
+    texts_right_indices = pad_sequences(texts_right_indices, maxlen=max_seq_len, padding='post', truncating='post')
+    texts_right_with_aspects_indices = tokenizer.texts_to_sequences(texts_right_with_aspects)
+    texts_right_with_aspects_indices = pad_sequences(texts_right_with_aspects_indices, maxlen=max_seq_len, padding='post', truncating='post')
+    aspects_indices = tokenizer.texts_to_sequences(aspects)
+    aspects_indices = pad_sequences(aspects_indices, maxlen=max_aspect_len)
+    polarities_matrix = K.eval(tf.one_hot(indices=polarities, depth=polarities_dim))
+
+    if mode == 'test':
+        return texts_raw_indices, texts_raw_without_aspects_indices, texts_left_indices, texts_left_with_aspects_indices, \
+               aspects_indices, texts_right_indices, texts_right_with_aspects_indices, polarities_matrix
+
+    embedding_matrix_file_name = '{0}_{1}_embedding_matrix.dat'.format(str(embedding_dim), type)
+    if os.path.exists(embedding_matrix_file_name):
+        print('loading embedding_matrix:', embedding_matrix_file_name)
+        embedding_matrix = pickle.load(open(embedding_matrix_file_name, 'rb'))
+    else:
+        print('loading word vectors...')
+        word_vec = load_word_vec(word_index, embedding_dim)
+        embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
+        print('building embedding_matrix:', embedding_matrix_file_name)
+        for word, i in word_index.items():
+            vec = word_vec.get(word)
+            if vec is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = vec
+        pickle.dump(embedding_matrix, open(embedding_matrix_file_name, 'wb'))
+
+    return texts_raw_indices, texts_raw_without_aspects_indices, texts_left_indices, texts_left_with_aspects_indices, \
+           aspects_indices, texts_right_indices, texts_right_with_aspects_indices, \
+           polarities_matrix, \
+           embedding_matrix, \
+           tokenizer
+
+if __name__ == '__main__':
+    read_dataset()
